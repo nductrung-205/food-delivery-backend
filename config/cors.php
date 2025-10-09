4bc33ef2051d46ca6ddfb816c4ed270a8d1c004a
Duc Trung <nductrung205@gmail.com>
2025-10-09 18:03:37 +0700

    Update CORS to allow Vercel domain

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,11 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'], // React app
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://food-delivery-frontend.onrender.com',
+        'https://cdtt-eight.vercel.app',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
