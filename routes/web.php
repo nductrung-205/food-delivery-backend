bf51c70d520a0f6538c1f52cbf6facf514bd1621
Duc Trung <nductrung205@gmail.com>
2025-10-15 15:57:22 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json(['message' => 'Laravel backend is running']);
+});
+
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+    return 'Storage link created successfully!';
 });
 
 
